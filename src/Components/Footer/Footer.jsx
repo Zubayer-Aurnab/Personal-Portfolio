c69bcceec63f05500f0a4cab5044a4df4204e53a
Aurnab <zubayeraurnab@gmail.com>
2024-03-24 22:00:57 +0600

    Achievements & Certificates slider added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa";
 import { GrLinkedin } from "react-icons/gr";
 const Footer = () => {
     return (
-        <footer className="footer p-2 lg:px-96 bg-neutral text-neutral-content flex justify-between items-center">
+        <footer className="footer p-2 lg:px-96 bg-[#1b233d] text-neutral-content flex justify-between items-center">
             <aside>
 
                 <p>© 2020 yitbd. All Rights Reserved. Design and developed by Zubayer Alam Aurnab</p>
@@ -10,11 +10,11 @@ const Footer = () => {
             <nav>
                 <header className="footer-title">Social</header>
                 <div className="grid grid-flow-col gap-4">
-                    <a href="https://github.com/Zubayer-Aurnab" target="_blank" className="text-4xl ">
+                    <a href="https://github.com/Zubayer-Aurnab" target="_blank" rel="noreferrer" className="text-4xl ">
                         <FaGithub />
                     </a>
 
-                    <a href="https://www.linkedin.com/in/zubayer-aurnab-500b0725a/" target="_blank" className="text-4xl ">
+                    <a href="https://www.linkedin.com/in/zubayer-aurnab-500b0725a/" target="_blank" rel="noreferrer" className="text-4xl ">
                         <GrLinkedin />
                     </a>
 
--- src/Components/ScroolUP/ScrollUp.jsx
+++ src/Components/ScroolUP/ScrollUp.jsx
@@ -1,16 +1,71 @@
 import { GrLinkTop } from "react-icons/gr";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+// import required modules
+import { FreeMode, Pagination, Autoplay } from 'swiper/modules';
+//images for slider
+import PH from '../../assets/pdfs/PNG/Programming Hero.png'
+import Presentation from '../../assets/pdfs/PNG/Presentation And PublicSpeaking Certificate.jpg'
+import Communication from '../../assets/pdfs/PNG/Communication Secrets.png'
+import Abriti from '../../assets/pdfs/PNG/জুবায়ের আলম অর্ণব.jpg'
 
 const ScrollUp = () => {
     function scrollToTop() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
+
     return (
-        <div className="flex justify-end my-10 px-7">
-            <div onClick={scrollToTop} className="btn">
+        <div className="lg:flex justify-between items-center my-10 w-4/5 mx-auto ">
+            <div className="lg:w-[50%] ">
+                <h1 className="text-white mb-2 lg:text-xl font">Achievements & Certificates</h1>
+                <Swiper
+                    slidesPerView={2}
+                    spaceBetween={20}
+                    freeMode={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[FreeMode, Pagination, Autoplay]}
+                    loop={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    className="mySwiper"
+                >
+                    <SwiperSlide className="" >
+                        <a href="https://drive.google.com/file/d/1EcioiJYmwxZk2Mz-JdHyKjn8THeo5C9t/view?usp=sharing" target="_blank" rel="noreferrer">
+                            <img src={PH} alt="Programming hero " className=" h-28 md:h-52 lg:h-72 object-cover" />
+                        </a>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <a href="https://drive.google.com/file/d/1C9Ns-8zTnFXLDcWTcMKwEUYi_TTK8cjA/view?usp=sharing" target="_blank" rel="noreferrer">
+                            <img src={Presentation} alt="" />
+                        </a>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <a href="https://drive.google.com/file/d/1Yxwt0wHLLa-k77qIHyaoHbVQLdlFfHg8/view?usp=sharing" target="_blank" rel="noreferrer">
+                            <img src={Communication} alt="" />
+                        </a>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <a href="https://drive.google.com/file/d/1hazdOQe2LiI-HnrCvHqSsA8a3tf0V2rr/view?usp=sharing" target="_blank" rel="noreferrer">
+                            <img src={Abriti} alt="" />
+                        </a>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+
+            <div onClick={scrollToTop} className="btn hidden lg:flex">
                 <GrLinkTop />
             </div>
-        </div>
+
+        </div >
     );
 };
 
